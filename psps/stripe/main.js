cff593c77980ac90e8083774d1310912f037b80e
Dominik Mengelt <dmengelt@google.com>
2024-04-17 10:53:14 +0200

    added stripe payment request button integration

---
+++ psps/stripe/main.js
@@ -0,0 +1,30 @@
+const stripe = Stripe('pk_test_LMUiQyn0mBZPsUIhVrVMblov', {
+    apiVersion: "2024-04-10",
+  });
+
+  const paymentRequest = stripe.paymentRequest({
+    country: 'US',
+    currency: 'usd',
+    total: {
+      label: 'Demo total',
+      amount: 1099,
+    },
+    requestPayerName: true,
+    requestPayerEmail: true,
+  });
+
+const elements = stripe.elements();
+const prButton = elements.create('paymentRequestButton', {
+  paymentRequest,
+});
+
+(async () => {
+  // Check the availability of the Payment Request API first.
+  const result = await paymentRequest.canMakePayment();
+  if (result) {
+    prButton.mount('#payment-request-button');
+  } else {
+    console.log("### canMakePayment returned false!");
+    document.getElementById('payment-request-button').style.display = 'none';
+  }
+})();
