3a06fe536112b50d5a3bd5b83003b2f641cc0b64
Dominik Mengelt <dmengelt@google.com>
2024-04-17 11:36:52 +0200

    removed requestShipping from the Stripe example again

--- psps/stripe/main.js
+++ psps/stripe/main.js
@@ -8,8 +8,7 @@ const stripe = Stripe('pk_test_LMUiQyn0mBZPsUIhVrVMblov', {
     total: {
       label: 'Demo total',
       amount: 1099,
-    },
-    requestShipping: true
+    }
   });
 
 const elements = stripe.elements();
