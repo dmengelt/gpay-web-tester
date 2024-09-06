ab22015fbd6403b8d7b425dd24130d1e7244357d
Dominik Mengelt <dmengelt@google.com>
2024-09-06 12:27:19 +0200

    added stripe express checkout element example

---
+++ psps/stripe/express-checkout-element.js
@@ -0,0 +1,28 @@
+const stripe = Stripe('pk_test_51Mg8nWA9tv049KWdCFQOwUElIVS3FvQLaGlugWfltUCuTjp4oj5oLkH5pq26W4cTr5GscxAgNaKAMA3z85j6GZUW00D54sM83r', {
+    apiVersion: "2024-04-10",
+});
+
+const appearance = {
+  theme: 'stripe',
+  variables: {
+    borderRadius: '36px',
+  }
+}
+
+const expressCheckoutOptions = {
+  buttonType: {
+    applePay: 'buy',
+    googlePay: 'buy',
+    paypal: 'buynow'
+  }
+}
+
+const elements = stripe.elements({
+  mode: 'payment',
+  amount: 1099,
+  currency: 'usd',
+  appearance,
+})
+
+const expressCheckoutElement = elements.create('expressCheckout', expressCheckoutOptions)
+expressCheckoutElement.mount('#express-checkout-element')
